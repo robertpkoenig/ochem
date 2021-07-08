7a08fc1ec9fa60b03ffa7e9a9fa4aabd77c9afd4
Robert Koenig <robertpkoenig@gmail.com>
2021-07-08 12:59:27 +0100

    landing page

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;600;700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
